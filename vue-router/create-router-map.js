8d92da43405b04ae6d6f3976199fcf24342322c1
hearsay316 <qazwsx2228@163.com>
2020-05-26 17:49:54 +0800

    创建路由

--- vue-router/create-router-map.js
+++ vue-router/create-router-map.js
@@ -1,3 +1,20 @@
-export default function createRouterMap( routes ){
+export default function createRouterMap( routes, oldList=[], oldMap={}){
     console.log()
+    let pathList = oldList;
+    let pathMap = oldMap;
+    routes.forEach(route=>{
+        addRouteRecord(route,pathList,pathMap)
+    })
+    return {
+        pathList,pathMap
+    }
+}
+function addRouteRecord(router, pathList, pathMap){
+    let path = router.path;
+    let record = {
+        path,
+        component:router.component
+    }
+    pathMap[path] = record
+    //pathList.push
 }
--- vue-router/createMatcher.js
+++ vue-router/createMatcher.js
@@ -2,14 +2,15 @@ import createRouteMap from './create-router-map.js'
 export default function createMatcher(routes) {
     console.log(routes,"createMatcher")
     let {pathList,pathMap}  = createRouteMap(routes)
-    function match() {
-
+    function match(url) {
+        let component =   pathMap.includes(url) && pathMap(url)
+        return component ?component :void 0
     }
-    function addRoutes() {
-
+    function addRoutes(routes) {
+        createRouteMap(routes,pathList,pathMap)
     }
     return {
-        match,
+        match ,
         addRoutes
     }
 }
