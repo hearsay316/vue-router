68ec7730923fd1e77ceaa4a70e8e1a0f317579ac
hearsay316 <qazwsx2228@163.com>
2020-06-09 16:16:02 +0800

    渲染视图

--- vue-router/comonents/router-view.js
+++ vue-router/comonents/router-view.js
@@ -1,6 +1,22 @@
 export default {
-    name:"router-view",
-    render(){
-        return <a>{this.$slots.default}</a>
+  name: "router-view",
+  functional: true, // 函数式组件
+  render(h, { parent, data }) {
+    console.log(h, parent, data);
+    let depth = 0;
+    let route = parent.$route;
+    data.routeView = true; // 标识路由属性
+    while (parent) {
+      if (parent.$vnode && parent.$vnode.data.routeView) {
+        depth++;
+      }
+      parent = parent.$parent;
     }
-}
+    console.log(depth, route);
+    const record = route.matched[depth];
+    if (!record) {
+      return h();
+    }
+    return h(record.component, data);
+  },
+};
