1816b92fff46f375ffad07e9de0ac392afab1df8
hearsay316 <qazwsx2228@163.com>
2020-06-09 16:47:30 +0800

    页面跳转

--- vue-router/comonents/router-link.js
+++ vue-router/comonents/router-link.js
@@ -1,6 +1,30 @@
 export default {
-    name: "router-link",
-    render(){
-        return <a>{this.$slots.default}</a>
-    }
-}
+  name: "router-link",
+  props: {
+    to: {
+      required: true,
+      type: String,
+    },
+    tag: {
+      type: String,
+    },
+  },
+  methods: {
+    handleClickPush() {
+      console.log("5555", this.to, this);
+      this.$router.push(this.to);
+    },
+  },
+  render(h) {
+    let tag = this.tag || "a";
+    return h(
+      tag,
+      {
+        on: {
+          click: this.handleClickPush,
+        },
+      },
+      this.$slots.default
+    );
+  },
+};
--- vue-router/index.js
+++ vue-router/index.js
@@ -1,42 +1,45 @@
 import install from "./install";
-import createMatcher from './create-matcher.js'
+import createMatcher from "./create-matcher.js";
 import BrowserHistory from "./history/browserHistory";
 import HashHistory from "./history/hashHistory";
-class VueRouter{
-    constructor(option){
-        const {routes} = option
-        this.matcher = createMatcher(routes||[]);
-        // 创建历史管理
-        this.mode = option.mode||"hash";
-        switch (this.mode){
-            case "hash":
-                this.history = new HashHistory(this)
-                break;
-            case "history":
-                this.history = new BrowserHistory(this)
-                break;
-        }
-
+class VueRouter {
+  constructor(option) {
+    const { routes } = option;
+    this.matcher = createMatcher(routes || []);
+    // 创建历史管理
+    this.mode = option.mode || "hash";
+    switch (this.mode) {
+      case "hash":
+        this.history = new HashHistory(this);
+        break;
+      case "history":
+        this.history = new BrowserHistory(this);
+        break;
     }
-    match(location){
-        return this.matcher.match(location)
-    }
-    init(app){
-        console.log(app)
-        const history = this.history;
-        const setupHashListener = ()=>{
-            history.setupListener();
-        }
-        // 跳转路径 会根据匹配来操作
-        history.transitionTo(history.getCurrentLocation(),setupHashListener)
-        history.listen((route)=>{
-             app._route = route
-        })
-        //
-        // transitionTo 跳转
-        // setupHashListener hash 才有的
-    }
+  }
+  match(location) {
+    return this.matcher.match(location);
+  }
+  init(app) {
+    console.log(app);
+    const history = this.history;
+    const setupHashListener = () => {
+      history.setupListener();
+    };
+    // 跳转路径 会根据匹配来操作
+    history.transitionTo(history.getCurrentLocation(), setupHashListener);
+    history.listen((route) => {
+      app._route = route;
+    });
+    //
+    // transitionTo 跳转
+    // setupHashListener hash 才有的
+  }
+  push(location) {
+    const history = this.history;
+    window.location.hash = location;
+  }
 }
 
-VueRouter.install = install
-export default VueRouter
+VueRouter.install = install;
+export default VueRouter;
